df3dac4a2b5116f5b2164dfdd1526bfb041dd409
MaphsterB <docmaph@gmail.com>
2017-04-13 00:02:55 -0400

    Add code from Slack SDK examples

--- bot.js
+++ bot.js
@@ -1,2 +1,24 @@
 #!node
 
+var SlackClient = require("@slack/client");
+var RtmClient = SlackClient.RtmClient;
+var RTM_EVENTS = SlackClient.RTM_EVENTS;
+
+var token = process.env.SLACK_API_TOKEN || "";
+
+
+var rtm = new RtmClient(token, { logLevel: "info"});
+rtm.start();
+
+
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+    console.log("Message Received: ", message);
+});
+
+rtm.on(RTM_EVENTS.REACTION_ADDED, function(reaction) {
+    console.log("Reaction Added: ", reaction);
+});
+
+rtm.on(RTM_EVENTS.REACTION_REMOVED, function(reaction) {
+    console.log("Reaction Removed: ", reaction);
+});
